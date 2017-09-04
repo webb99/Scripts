9f42abb0287c3ea7871f754233d2cf85a2673992
webb99 <webb_jack99@outlook.com>
2017-09-04 16:19:07 +0100

    remove

--- Useful Administrator Scripts/Set - IPv6 Disabled.ps1
+++
@@ -1,5 +0,0 @@
-$nic = get-netadapter
-Disable-NetAdapterBinding –InterfaceAlias $nic.name –ComponentID ms_tcpip6 -Verbose
-Set-NetTeredoConfiguration -Type Disabled -Verbose
-Set-NetIsatapConfiguration -State Disabled -Verbose
-Set-Net6to4Configuration -State Disabled -Verbose
--- Useful Administrator Scripts/Set - SMBv1 Disabled.ps1
+++
@@ -1,3 +0,0 @@
-Set-SmbServerConfiguration -EnableSMB1Protocol $False -Force
-Set-SmbServerConfiguration -EnableSMB2Protocol $True -Force
-Remove-WindowsFeature -Name FS-SMB1 -Remove
